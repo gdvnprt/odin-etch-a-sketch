a7bcea908f6a2dd7f9d197efc8116205063dcbfd
Gavin Davenport <gavinkdavenport@gmail.com>
2023-10-04 13:49:02 -0700

    Create etching
    Can draw over pixels with mouse

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const container = document.querySelector("#container");
 
 function makePixels(a) {
     let b = Math.pow(a, 2);
-    let dimensions = (800 / a) + "px"
+    let dimensions = (800 / a) + "px";
     for (let i = 0; i < b; i++) {
         let etchPixel = document.createElement('div');
         etchPixel.classList.add('etchPixel');
         etchPixel.style.width = dimensions;
         etchPixel.style.height = dimensions;
         container.appendChild(etchPixel);
+
+        etchPixel.addEventListener('mouseover', () => {
+            etchPixel.style.backgroundColor = "black";
+        })
     }
 }
